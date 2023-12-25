aae1df5948e02f7f67e5d49b924d02766b5f6ac1
HemantaPradhan <152636364+HemantaPradhan@users.noreply.github.com>
2023-12-25 20:13:27 +0530

    b

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
-from flask import Flask
+def add(x, y):
+    return x + y
 
-app = Flask(__name__)
+def subtract(x, y):
+    return x - y
 
-@app.route('/')
-def home():
-    return 'Welcome to My Python Web App!'
+def multiply(x, y):
+    return x * y
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero"
+
+# Main calculator loop
+while True:
+    # User input
+    num1 = float(input("Enter first number: "))
+    operation = input("Enter operation (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+
+    # Perform calculation based on user input
+    if operation == '+':
+        result = add(num1, num2)
+    elif operation == '-':
+        result = subtract(num1, num2)
+    elif operation == '*':
+        result = multiply(num1, num2)
+    elif operation == '/':
+        result = divide(num1, num2)
+    else:
+        print("Invalid operation. Please enter +, -, *, or /.")
+        continue
+
+    # Display result
+    print("Result: {}".format(result))
+
+    # Ask the user if they want to perfo
+
 
 
